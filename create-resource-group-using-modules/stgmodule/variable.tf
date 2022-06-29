70d448979649d65930a24fda08e811ca1de76703
YASWANT KUMAR SINGHI <40240984+Yaswant-Kumar-Singhi@users.noreply.github.com>
2022-06-30 00:06:11 +0530

    Create variable.tf

---
+++ create-resource-group-using-modules/stgmodule/variable.tf
@@ -0,0 +1,18 @@
+variable "resourcegroup" {
+  type = string
+  default = "terraform-module-rg"
+}
+
+variable "resourcegrouplocation" {
+  type = string
+  default = "Central India"
+}
+
+variable "resourcegrouptags" {
+  type = map(string)
+  default = {
+    "Environment" = "DevelopmentModules"
+    "Dept" = "trainingonmodules"
+  }
+   
+}
