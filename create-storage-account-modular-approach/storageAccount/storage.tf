76593a90bdb1192791805921a62443628721d9ff
YASWANT KUMAR SINGHI <40240984+Yaswant-Kumar-Singhi@users.noreply.github.com>
2022-07-02 13:07:18 +0530

    Update storage.tf

--- create-storage-account-modular-approach/storageAccount/storage.tf
+++ create-storage-account-modular-approach/storageAccount/storage.tf
@@ -1,9 +1,9 @@
 module "kthServiceStorageAccount" {
-    source = "../modules/storage_v1"
-    storageaccountname = "storageaccountterra001"
-    resourcegroupname = module.kthServices.azurerm_resource_group_name
-    storageaccountlocation =  module.kthServices.azurerm_resource_group_location
-    storageaccountaccounttier = var.storageTier
-    storageaccountaccountreplicationtype = var.storageReplication
-    storageaccounttags = var.resourcegrouptags
+    source                                   = "../modules/storage_v1"
+    storageaccountname                       = "storageaccountterra001"
+    resourcegroupname                        = module.kthServices.azurerm_resource_group_name            #Refer Resource group for this
+    storageaccountlocation                   =  module.kthServices.azurerm_resource_group_location       #Refer Resource group for this
+    storageaccountaccounttier                = var.storageTier
+    storageaccountaccountreplicationtype     = var.storageReplication
+    storageaccounttags                       = var.resourcegrouptags
 }
